4e7387faf8189f86b462a20932b78b76ad50c64f
elham saremi <elhamsaremi88@gmail.com>
2023-08-10 03:59:40 +0330

    add the html and js file about question 4

---
+++ q4.js
@@ -0,0 +1,55 @@
+const notificationBoxHTML = `
+  <div class="notificationBox">
+    <p class="message"></p>
+    <div class="close">
+      <svg
+        width="16"
+        height="16"
+        viewBox="0 0 16 16"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M15 1L8 8M8 8L1 15M8 8L1 1M8 8L15 15"
+          stroke="#084108"
+          strokeWidth="2"
+          strokeLinecap="round"
+        />
+      </svg>
+    </div>
+  </div>
+`;
+
+const tempElement = document.createElement("div");
+tempElement.innerHTML = notificationBoxHTML;
+
+const notificationBox = tempElement.querySelector(".notificationBox");
+document.body.appendChild(notificationBox);
+
+const messageElement = notificationBox.querySelector(".message");
+const btn = document.querySelector(".notificationBtn");
+
+function showNotification(message, position, background, className) {
+  // debugger;
+  notificationBox.style.top = position.top + "px";
+  notificationBox.style.right = position.right + "px";
+  notificationBox.style.background = background;
+  notificationBox.classList.add = className;
+  messageElement.textContent = message;
+  notificationBox.style.display = "flex";
+}
+
+btn.addEventListener("click", () => {
+  // debugger;
+  showNotification(
+    "This is the notification message!",
+    { top: 50, right: 10 },
+    "yellow",
+    "success"
+  );
+});
+
+const closeButton = document.querySelector(".close");
+closeButton.addEventListener("click", () => {
+  notificationBox.style.display = "none";
+});
