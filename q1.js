d2eebfb9b4b239f3e70b7a8ed527b323b281dffc
elham saremi <elhamsaremi88@gmail.com>
2023-08-10 03:44:37 +0330

    add the js file of question one

--- q1.js
+++ q1.js
@@ -3,23 +3,26 @@
 let user = {
   name: "john",
   age: 30,
+  sayHello() {
+    console.log(`hello ${this.name}`);
+  },
 };
 
-function sayHello() {
-  console.log(`hello ${this.name}`);
-}
+// function sayHello() {
+//   console.log(`hello ${this.name}`);
+// }
+// user.sayHello = sayHello;
 
-//   user.sayHello = function () {
-//     console.log(`hello ${this.name}`);
-//   };
+// user.sayHello = function () {
+//   console.log(`hello ${this.name}`);
+// };
 
-user.sayHello = sayHello;
-user.sayHello();
-// let user = {
+// let user2 = {
 //   name: "john",
 //   sayHello: () => {
 //     console.log(`hello ${user.name}`);
 //   },
 // };
+// user2.sayHello();
 
-// user.sayHello();
+user.sayHello();
